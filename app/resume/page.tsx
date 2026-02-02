1d9a38149a37dcf25624078f009a78a8ae786013
ryjpete <ryan.j.peterson.80@gmail.com>
2026-02-02 12:22:54 -0600

    ui: update copy

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -24,15 +24,15 @@ export default function ResumePage() {
   const intro = (
     <section className={styles.intro}>
       <h1>Ryan Peterson</h1>
-      <h2>Senior Software Developer</h2>
-      <h3>React enthusiast. CSS superhero. Overall nerd.</h3>
+      <h2>UI Engineer.</h2>
+      <h3>Frontend Software Dev. React enthusiast. CSS superhero. Overall nerd.</h3>
     </section>
   );
 
   const summary = (
     <section className={styles.summary}>
       <p>
-        Howdy. I&apos;m Ryan. I&apos;m a senior / lead / principal ... I&apos;m a developer. I have 20+ years of experience designing and building web applications and digital products across various industries using a plethora of technologies. My primary focus is on the frontend, though I&apos;m not opposed to jumping into that full-stack arena.
+        Hey there. I&apos;m Ryan. It&apos;s wild how many titles exist for what I&apos;ve been up to for the past two decades. But the one that fits best would probably be <strong>UI Engineer</strong>. I fit right at that intersection of design and developmnent. My education and early career started from the design world, but I quickly found passion in building and styling digital things. Figma and React are my best friends, and the challenge of creating parity between the two is what gets me going. That&apos;s not to say deep-diving on creative mockups and backend development isn&apos;t something I&apos;m interested in. It is. It&apos;s just that my sweet spot has always been somewhere in the middle. I love crafting clean code with streamlined styling that is flawless across the board. The goal is to hand off code to the backend team that is seamless to integrate with.
       </p>
     </section>
   );
