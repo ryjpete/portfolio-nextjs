34f357a4c1cad7d18265999ac69ff17af8ac8bba
ryjpete <ryan.j.peterson.80@gmail.com>
2026-03-07 20:14:16 -0600

    feature: storybook install; setup IconButton storybook el

---
+++ src/components/icon-button/index.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Image from "next/image";
+
+interface IconButtonProps {
+  onClick: () => void;
+  src: string;
+  ariaLabel?: string;
+}
+
+export default function IconButton({ 
+  onClick,
+  ariaLabel = "",
+  src
+}: IconButtonProps) {
+  if (!src) return false;
+
+  return (
+    <button
+      type="button"
+      aria-label={ariaLabel}
+      onClick={onClick}
+    >
+      <Image
+        src={src}
+        alt={ariaLabel}
+        width={48}
+        height={48}
+      />
+    </button>
+  );
+}
