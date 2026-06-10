859678d7719333c6eda1f6499c61a4c1f8ed43c5
ryjpete <ryan.j.peterson.80@gmail.com>
2026-06-10 10:05:22 -0500

    cleanup

--- src/app/(project)/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function ProjectLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="container">
-      {children}
-    </div>
-  );
-}
--- src/app/api/hubble-feed/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET() {
-  const res = await fetch("https://esahubble.org/images/feed/category/nebulae/");
-  const xml = await res.text();
-  return new Response(xml, {
-    headers: { "Content-Type": "application/xml" },
-  });
-}
